0f348ec84d039cd9081edb693c65b50a98f960f8
icanmakeit0k <229132252+helloSokchea@users.noreply.github.com>
2025-08-28 20:23:02 +0700

    .

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Looks for classes in the root index.html
+    "./src/**/*.{js,css}", // Looks for classes in any JS or CSS files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
